80cd25c8f2d971f5cf2d7d2fb6138c03d97975bf
Simon Duck <simon.duck@softwire.com>
2025-04-23 12:26:36 +0100

    Regex solution - Regex includes extra characters in array

--- EmailExtraction.js
+++ EmailExtraction.js
@@ -1,16 +1,7 @@
 let fs = require("fs");
 let fileName = "test.txt";
 
-try {
-  let data = fs.readFileSync(fileName, "utf8");
-  let text = data.toString();
-  let counter = 0;
-  for (let i = 0; i < text.length; i++) {
-    if (text.substring(i, i + 13) === "@softwire.com") {
-      counter++;
-    }
-  }
-  console.log(counter);
-} catch (e) {
-  console.log("Error: ", e.stack);
-}
+let data = fs.readFileSync(fileName, "utf8");
+let matchArray = data.match(/\S\w\D\S*@softwire.com\W\S\w/gm); 
+console.log(matchArray.length);
+console.log(matchArray);
