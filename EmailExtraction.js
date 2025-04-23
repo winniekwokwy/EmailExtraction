255d2dd7a169ed0a51fe4299028a36715315d7fe
Winnie Kwok <winnie.kwok@softwire.com>
2025-04-23 15:43:05 +0100

    Fix the issue with hotmail.co.uk r

--- EmailExtraction.js
+++ EmailExtraction.js
@@ -2,6 +2,35 @@ let fs = require("fs");
 let fileName = "test.txt";
 
 let data = fs.readFileSync(fileName, "utf8");
-let matchArray = data.match(/\S+@softwire.com\b/gm); 
-console.log(matchArray.length);
-console.log(matchArray.join('\n'));
+//let matchArray = data.match(/^[a-zA-Z0-9._%±]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}/gm);
+let matchArray = data.match(/@[a-zA-Z0-9.-]+\.[a-zA-Z.-]+\b/gm);
+//console.log(matchArray);
+
+let dictionary = {};
+if (matchArray.length>=1){
+    matchArray.map((domain)=>{
+        let keys = Object.keys(dictionary);
+        if (dictionary.length===0 || keys.indexOf(domain)==-1 ){
+            dictionary[domain]=1;
+        }
+        else {
+            dictionary[domain]+= 1;
+        }
+    })
+}
+console.log(dictionary);
+const sortedDict = Object.fromEntries(
+    Object.entries(dictionary).sort(([,a],[,b]) => b- a)
+);
+//console.log(sortedDict);
+
+let counter = 0;
+
+for (domain in sortedDict){
+    if (counter<=9){
+        if (sortedDict.hasOwnProperty(domain))
+            console.log(`domain: ${domain}: count: ${sortedDict[domain]}`);
+        counter++;
+    }
+}
+
