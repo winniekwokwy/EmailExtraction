8f274cc680e86e644dad26b8478eb494797d0f80
Simon Duck <simon.duck@softwire.com>
2025-04-24 10:39:02 +0100

    Adding to my Github

--- EmailExtraction.js
+++ EmailExtraction.js
@@ -7,30 +7,28 @@ let matchArray = data.match(/@[a-zA-Z0-9.-]+\.[a-zA-Z.-]+\b/gm);
 //console.log(matchArray);
 
 let dictionary = {};
-if (matchArray.length>=1){
-    matchArray.map((domain)=>{
-        let keys = Object.keys(dictionary);
-        if (dictionary.length===0 || keys.indexOf(domain)==-1 ){
-            dictionary[domain]=1;
-        }
-        else {
-            dictionary[domain]+= 1;
-        }
-    })
+if (matchArray.length >= 1) {
+  matchArray.map((domain) => {
+    let keys = Object.keys(dictionary);
+    if (dictionary.length === 0 || keys.indexOf(domain) == -1) {
+      dictionary[domain] = 1;
+    } else {
+      dictionary[domain] += 1;
+    }
+  });
 }
 console.log(dictionary);
 const sortedDict = Object.fromEntries(
-    Object.entries(dictionary).sort(([,a],[,b]) => b- a)
+  Object.entries(dictionary).sort(([, a], [, b]) => b - a)
 );
 //console.log(sortedDict);
 
 let counter = 0;
 
-for (domain in sortedDict){
-    if (counter<=9){
-        if (sortedDict.hasOwnProperty(domain))
-            console.log(`domain: ${domain}: count: ${sortedDict[domain]}`);
-        counter++;
-    }
+for (domain in sortedDict) {
+  if (counter <= 9) {
+    if (sortedDict.hasOwnProperty(domain))
+      console.log(`domain: ${domain}: count: ${sortedDict[domain]}`);
+    counter++;
+  }
 }
-
